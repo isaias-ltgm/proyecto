fa53354121d7a3a818c9d06d0d49103f82173ccf
isaias-ltgm <leo.diazmurrieta29@gmail.com>
2026-04-30 00:39:37 -0500

    igi

---
+++ backend/src/main/java/com/senati/BodegaPepito/BodegaPepitoApplication.java
@@ -0,0 +1,13 @@
+package com.senati.BodegaPepito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodegaPepitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodegaPepitoApplication.class, args);
+	}
+
+}
