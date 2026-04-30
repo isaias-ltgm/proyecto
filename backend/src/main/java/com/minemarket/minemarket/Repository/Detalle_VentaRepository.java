7d0427e6ddc75ba39dca1f34ad6957627f09d964
isaias-ltgm <leo.diazmurrieta29@gmail.com>
2026-04-30 00:37:02 -0500

    tuu

--- backend/src/main/java/com/minemarket/minemarket/Repository/Detalle_VentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.minemarket.minemarket.Repository;
-
-import  com.minemarket.minemarket.Entity.Detalle_venta;
-import  org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Detalle_VentaRepository  extends JpaRepository<Detalle_venta,Long>{
-}
--- backend/src/main/java/com/minemarket/minemarket/Repository/VentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.minemarket.minemarket.Repository;
-
-import com.minemarket.minemarket.Entity.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VentaRepository extends JpaRepository<Venta,Long> {
-}
